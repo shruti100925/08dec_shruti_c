6c4900fb92f7a1ca51f3da05f8afa687ec61d336
shruti100925 <120240503+shruti100925@users.noreply.github.com>
2022-12-14 16:12:48 +0530

    Add files via upload

---
+++ Assignment/Module-1/Hello.c
@@ -0,0 +1,14 @@
+//WAP to print Hello world
+#include<stdio.h>
+void main ()
+{
+    printf("Hello world");
+    printf("\nHello \nworld");
+    printf("\nHello \tworld");
+    printf("\nHello\v world");
+    printf("\nHelloo\b world");
+    printf("\nHello world\?");
+    printf("\n\"Hello world\"");
+    printf("\n\'Hello world'");
+    printf("\nHello\a world");
+}
