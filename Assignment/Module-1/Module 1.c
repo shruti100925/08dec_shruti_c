4982a1a90c077dc4d9c0281a1d81bd6f8e60341a
shruti100925 <120240503+shruti100925@users.noreply.github.com>
2022-12-16 09:48:46 +0530

    Add files via upload

---
+++ Assignment/Module-1/Module 1.c
@@ -0,0 +1,9 @@
+//WAP to print person's introduction
+#include<stdio.h>
+void main()
+{
+	printf("Name:Shruti gadhavi");
+	printf("\nBirth date:25\\10\\1993");
+	printf("\nAge:28");
+	printf("\nAddress:kailashdhara park soc., raiya road");
+}
